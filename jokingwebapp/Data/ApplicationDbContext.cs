2fb4f8289b73f595b43124386b64f3129b5f13f1
that_ <that_@DESKTOP-EP2BMT7>
2022-07-30 17:53:20 -0500

    Add project files.

---
+++ jokingwebapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jokingwebapp.Models;
+
+namespace jokingwebapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jokingwebapp.Models.Joke>? Joke { get; set; }
+    }
+}
